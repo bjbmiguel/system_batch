900a655e0849ce379fb05bdb1ec135ae31d218ed
Bruno Banguela Miguel <bruno.banguela@unitel.co.ao>
2024-06-20 19:33:41 +0100

    first commit

---
+++ src/main/java/ao/co/unitel/dl/dsi/dqsso/oss/system_batch/SystemBatchApplication.java
@@ -0,0 +1,13 @@
+package ao.co.unitel.dl.dsi.dqsso.oss.system_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/co/unitel/dl/dsi/dqsso/oss/system_batch/SystemBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.co.unitel.dl.dsi.dqsso.oss.system_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
